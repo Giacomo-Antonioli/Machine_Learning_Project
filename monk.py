a4e0fe241319b748e0119a71950ee4467a44ea5e
Paolo Fasano <super-pablo@hotmail.it>
2021-12-06 17:32:31 +0100

    set return fit to true in monk.py

--- monk.py
+++ monk.py
@@ -5,6 +5,7 @@
 #  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+from multiprocessing import freeze_support
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
@@ -16,7 +17,6 @@ from NeuralNetworkCore.Optimizers import RMSProp,StochasticGradientDescent
 from NeuralNetworkCore.Validation.Model_selection import GridSearch
 from NeuralNetworkCore.Utils.LoadCSVData import LoadCSVData
 
-import wandb
 
 columns = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
 for monk in ['monks-1','monks-2','monks-3']:
@@ -24,8 +24,8 @@ for monk in ['monks-1','monks-2','monks-3']:
     monk_test= monk+ '.test'
 
     print(os.getcwd())
-    monk_dataset, monk_labels= LoadCSVData.loadCSV(path = "./datasets/monks/", file_name = str(monk_train), separator=' ', column_names=columns, column_for_label='class')   
-    monk_dataset_test, monk_labels_test= LoadCSVData.loadCSV(path = "./datasets/monks/", file_name = str(monk_test), separator=' ', column_names=columns, column_for_label='class')
+    monk_dataset, monk_labels= LoadCSVData.loadCSV(path = "./datasets/monks/", file_name = str(monk_train), separator=' ', column_names=columns, column_for_label='class', returnFit=True)   
+    monk_dataset_test, monk_labels_test= LoadCSVData.loadCSV(path = "./datasets/monks/", file_name = str(monk_test), separator=' ', column_names=columns, column_for_label='class', returnFit=True)
 
     model = Model(monk)
     model.set_input_shape(17)
@@ -34,22 +34,25 @@ for monk in ['monks-1','monks-2','monks-3']:
     optimizer = StochasticGradientDescent()
     model.compile(optimizer=optimizer, metrics='binary', loss='squared')
     model.showLayers()
-    gridsearch_1 = GridSearch(model,
+    
+    if __name__ == '__main__':
+        freeze_support()
+        gridsearch_1 = GridSearch(model,
                               { 'opt': ['sgd','rmsprop'],'mom':[0.1,0.2,0.3,0.4,0.5,0.9], 'lr':[0.3,0.4,0.5,0.6,0.7,0.8,0.9] ,'metrics':['binary'], 'loss':['squared']}
                               )
-    gridsearch_1.fit(monk_dataset, monk_labels, epochs=200, batch_size=10, shuffle=False, cv=3,filename=monk)
-    # best_1=gridsearch_1.best_model
-    # int_test_1=best_1.evaluate(monk_dataset_test,monk_labels_test)
-    # print("#######################################")
-    # print(monk)
-    # print("Best TR metric")
-    # print(gridsearch_1.best_tr_metric)
-    # print("Best TR loss")
-    # print(gridsearch_1.best_tr_loss)
-    # print("Best Int metric")
-    # print(int_test_1[1])
-    # print("Best Int Loss")
-    # print(int_test_1[0])
-    # print("Best params")
-    # print(gridsearch_1.best_params)
-    # input()
+        gridsearch_1.fit(monk_dataset, monk_labels, epochs=200, batch_size=10, shuffle=False, cv=3,filename=monk)
+        best_1=gridsearch_1.best_model
+        int_test_1=best_1.evaluate(monk_dataset_test,monk_labels_test)
+        print("#######################################")
+        print(monk)
+        print("Best TR metric")
+        print(gridsearch_1.best_tr_metric)
+        print("Best TR loss")
+        print(gridsearch_1.best_tr_loss)
+        print("Best Int metric")
+        print(int_test_1[1])
+        print("Best Int Loss")
+        print(int_test_1[0])
+        print("Best params")
+        print(gridsearch_1.best_params)
+        input()
