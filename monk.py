d7b7b714f2d3815bffda5b0537b459b348e75a66
Malio Li <60041818+xiaoli98@users.noreply.github.com>
2022-01-18 22:07:42 +0100

    Add files via upload

--- monk.py
+++ monk.py
@@ -22,20 +22,30 @@ from NeuralNetworkCore.Utils.LoadCSVData import LoadCSVData
 
 columns = ['class', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'Id']
 
-#parameters
+#hyper-parameters
 opt = ['adam', 'rmsprop', 'sgd']
-mom = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-lr = [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
-metrics = ['binary', 'TF']
+mom = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+lr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+metrics = ['mee']
 loss = ['squared']
-beta1 = [0.95, 0.9, 0.85, 0.8]
-beta2 = [0.999, 0.995, 0.99, 0.95]
+beta1 = [0.95, 0.9]
+beta2 = [0.999, 0.995]
 epsilon = [1e-9, 1e-8]
-rho = [0.95, 0.9, 0.85, 0.8]
+rho = [0.95, 0.9]
 
-epochs = 200
+n_layer = 2
+n_unit_1 = [20]
+n_unit_2 = [1]
+ac_fun_1 = ['tanh']
+ac_fun_2 = ['sigmoid']
+weight_init = ['glorot_normal']
+bias_init = ['glorot_normal']
+drop_percent = [0.3]
+epochs = 100
 cross_validation = 5
-batch_size = 15
+batch_size = [1, 5]
+reg = ['l1']
+reg_param = [0.0003]
 
 for monk in ['monks-1']:#, 'monks-2', 'monks-3'
     monk_train = monk + '.train'
@@ -48,11 +58,11 @@ for monk in ['monks-1']:#, 'monks-2', 'monks-3'
 
     model = Model(monk)
     model.set_input_shape(17)
-    model.create_net(num_layer = 4, drop_frequency=1, num_unit=[4], act_func=['tanh', 'sigmoid'], weight_init= ['glorot_uniform', 'glorot_normal'],
-                     bias_init=['glorot_normal', 'glorot_uniform'], drop_percentage=[0.3, 0.4], drop_seed=[10])
+    model.create_net(num_layer = n_layer, drop_frequency=1, num_unit=[30], act_func=['tanh', 'sigmoid'], weight_init= ['glorot_normal'],
+                     bias_init=['glorot_normal'], drop_percentage=[0.3, 0.4], drop_seed=[10])
     
-    #model.add(Dense(4, activation_function='tanh'))
-    #model.add(Dense(1, activation_function='sigmoid'))
+    #model.add(Dense(30, activation_function='tanh'))
+    #model.add(Dense(40, activation_function='sigmoid'))
     optimizer = StochasticGradientDescent()
     model.compile(optimizer=optimizer, metrics='binary', loss='squared')
     
@@ -64,10 +74,23 @@ for monk in ['monks-1']:#, 'monks-2', 'monks-3'
                                'loss': loss,
                                'b1': beta1,
                                'b2': beta2,
-                               'e': epsilon,
-                               'rho': rho
+                               'epsilon': epsilon,
+                               'rho': rho,
+                               'weightinit_1': weight_init,
+                               'weightinit_2': weight_init,
+                               'biasinit_1': bias_init,
+                               'biasinit_2': bias_init,
+                               'actfun_1': ac_fun_1,
+                               'actfun_2': ac_fun_2,
+                               'units_1': n_unit_1,
+                               'units_2': n_unit_2,
+                               'batchsize': batch_size,
+                               'reg_1': reg,
+                               'regparam_1': reg_param,
+                               'reg_2': reg,
+                               'regparam_2': reg_param
                                }
-                              )
+                            )
     
     if __name__ == '__main__':
         gridsearch_1.fit(monk_dataset, monk_labels, epochs=epochs, batch_size=batch_size, shuffle=True, cv=cross_validation)
